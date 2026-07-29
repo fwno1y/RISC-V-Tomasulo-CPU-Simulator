f038c6d9137e817d5cffc5704c7119ae9737f52d
fwno1y <20070119@sjtu.edu.cn>
2026-07-29 09:33:28 +0800

    complete Reg.h

--- include/Reg.h
+++ include/Reg.h
@@ -1,8 +1,25 @@
-//
-// Created by 36433 on 2026/7/28.
-//
-
 #ifndef RISC_V_CPU_REG_H
 #define RISC_V_CPU_REG_H
+#include <cstdint>
+
+struct RegState {
+    uint32_t val[32] = {0};
+    uint32_t rob[32]{};
+
+    RegState() {
+        for (int i = 0; i < 32; ++i) {
+            rob[i] = -1;
+        }
+    }
+};
+
+class Register {
+    RegState cur;
+    RegState next;
+
+public:
+    void update();
+    void clear();
+};
 
 #endif //RISC_V_CPU_REG_H
