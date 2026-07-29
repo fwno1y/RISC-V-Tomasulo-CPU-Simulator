910efa6704444670d6766f8233213e5f407ab45f
fwno1y <20070119@sjtu.edu.cn>
2026-07-29 09:40:22 +0800

    complete Reg.cpp

--- src/Reg.cpp
+++ src/Reg.cpp
@@ -1,3 +1,17 @@
-//
-// Created by 36433 on 2026/7/28.
-//
+#include "../include/Reg.h"
+
+void Register::update() {
+    cur = next;
+    cur.val[0] = 0;
+    cur.rob[0] = -1;
+    next.val[0] = 0;
+    next.rob[0] = -1;
+}
+
+void Register::clear() {
+    for (int i = 0; i < 32; ++i) {
+        cur.rob[i] = -1;
+        next.rob[i] = -1;
+    }
+}
+
