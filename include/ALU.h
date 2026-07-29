04701944db1045777fddfc26e5a0b4e38fe8e1f7
fwno1y <20070119@sjtu.edu.cn>
2026-07-29 09:55:12 +0800

    complete ALU.h

--- include/ALU.h
+++ include/ALU.h
@@ -1,8 +1,14 @@
-//
-// Created by 36433 on 2026/7/28.
-//
-
 #ifndef RISC_V_CPU_ALU_H
 #define RISC_V_CPU_ALU_H
+#include <cstdint>
+
+#include "Instruction.h"
+
+class ALU {
+public:
+    ALU() = default;
+    static uint32_t execute(InstructionType op, uint32_t op1, uint32_t op2, uint32_t pc, uint32_t imm);
+    static bool execute_branch(InstructionType op, uint32_t op1, uint32_t op2);
+};
 
 #endif //RISC_V_CPU_ALU_H
